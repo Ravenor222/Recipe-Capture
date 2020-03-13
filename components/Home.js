85835629e17c43eca730311e29b5eca43d9c6ff8
Olivia <oliviapower.02@gmail.com>
2020-03-13 14:06:45 -0700

    add helper folder

--- components/Home.js
+++ components/Home.js
@@ -2,19 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import { FontAwesome } from '@expo/vector-icons';
-
-function identifyImage(imageData) {
-  // Initialise the Clarifai api
-  const Clarifai = require('clarifai');
-  const app = new Clarifai.App({
-      apiKey: '73cb02089cfe46e093f094aa426a0692'
-  });
-  // Identify the image
-  // this.displayAnswer(response.outputs[0].data.concepts[0].name
-  app.models.predict('bd367be194cf45149e75f01d59f77ba7', {base64: imageData})
-  .then((response) => console.log(response.outputs[0].data.concepts))
-  .catch((err) => console.log(err))
-}
+import identifyImage from './helpers/clarifai_helper'
 
 export default function Home (props){
   const [hasPermission, setHasPermission] = useState(null);
