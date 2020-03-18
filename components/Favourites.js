2a7034e4d72c309c1e58b5f2fc13460d48d5e796
Nick Danvers <ndanvers222@gmail.com>
2020-03-18 15:39:49 -0700

    favourites now rendering favourites from async storage

--- components/Favourites.js
+++ components/Favourites.js
@@ -5,103 +5,6 @@ import { Card, Block, NavBar, Icon, theme, Button } from 'galio-framework';
 
 const { width } = Dimensions.get('screen');
 
-const recipes = [{ 
-  id: 1,
-  title: 'Chicken Caesar Salad with Grilled Romaine',
-  readyInMinutes: 10,
-  image: 'https://spoonacular.com/recipeImages/593906-312x231.jpg',
-  imageType: 'jpg',
-  summary:
- 'The recipe Chicken Caesar Salad with Grilled Romaine is ready <b>in roughly 10 minutes</b> and is definitely a spectacular <b>gluten free and primal</b> option for lovers of American food. One serving contains <b>559 calories</b>, <b>18g of protein</b>, and <b>48g of fat</b>. For <b>$2.63 per serving</b>, this recipe <b>covers 33%</b> of your daily requirements of vitamins and minerals. It will be a hit at your <b>The Fourth Of July</b> event. If you have olive oil, sea salt, garlic, and a few other ingredients on hand, you can make it. To use up the olive oil you could follow this main course with the <a href="https://spoonacular.com/recipes/sauteed-banana-granola-and-yogurt-parfait-624619">Sauteed Banana, Granolan and Yogurt Parfait</a> as a dessert. Plenty of people made this recipe, and 152 would say it hit the spot. It works well as a main course.',
-  ingredients: ["1 tbsp butter", "2 tbsp grated cheese (I used romano)", "1-2 tbsp extra virgin olive oil", "5-6 cloves garlic", "6-8 ounces pasta (I used linguine)"],
-  instructions: [
-    {
-        "number": 1,
-        "step": "Preheat the oven to 200 degrees F."
-    },
-    {
-        "number": 2,
-        "step": "Whisk together the flour, pecans, granulated sugar, light brown sugar, baking powder, baking soda, and salt in a medium bowl."
-    },
-    {
-        "number": 3,
-        "step": "Whisk together the eggs, buttermilk, butter and vanilla extract and vanilla bean in a small bowl."
-    }
-  ]
- },
- { 
-  id: 2,
-  title: 'Chicken Caesar Salad with Grilled Romaine',
-  readyInMinutes: 20,
-  image: 'https://spoonacular.com/recipeImages/593906-312x231.jpg',
-  imageType: 'jpg',
-  summary:
-  'The recipe Chicken Caesar Salad with Grilled Romaine is ready <b>in roughly 10 minutes</b> and is definitely a spectacular <b>gluten free and primal</b> option for lovers of American food. One serving contains <b>559 calories</b>, <b>18g of protein</b>, and <b>48g of fat</b>. For <b>$2.63 per serving</b>, this recipe <b>covers 33%</b> of your daily requirements of vitamins and minerals. It will be a hit at your <b>The Fourth Of July</b> event. If you have olive oil, sea salt, garlic, and a few other ingredients on hand, you can make it. To use up the olive oil you could follow this main course with the <a href="https://spoonacular.com/recipes/sauteed-banana-granola-and-yogurt-parfait-624619">Sauteed Banana, Granolan and Yogurt Parfait</a> as a dessert. Plenty of people made this recipe, and 152 would say it hit the spot. It works well as a main course.',
-   ingredients: ["1 tbsp butter", "2 tbsp grated cheese (I used romano)", "1-2 tbsp extra virgin olive oil", "5-6 cloves garlic", "6-8 ounces pasta (I used linguine)"],
-   instructions: [
-     {
-         "number": 1,
-         "step": "Preheat the oven to 200 degrees F."
-     },
-     {
-         "number": 2,
-         "step": "Whisk together the flour, pecans, granulated sugar, light brown sugar, baking powder, baking soda, and salt in a medium bowl."
-     },
-     {
-         "number": 3,
-         "step": "Whisk together the eggs, buttermilk, butter and vanilla extract and vanilla bean in a small bowl."
-     }
-   ]
- },
- { 
-  id: 3,
-  title: 'Chicken Caesar Salad with Grilled Romaine',
-  readyInMinutes: 15,
-  image: 'https://spoonacular.com/recipeImages/593906-312x231.jpg',
-  imageType: 'jpg',
-  summary:
-  'The recipe Chicken Caesar Salad with Grilled Romaine is ready <b>in roughly 10 minutes</b> and is definitely a spectacular <b>gluten free and primal</b> option for lovers of American food. One serving contains <b>559 calories</b>, <b>18g of protein</b>, and <b>48g of fat</b>. For <b>$2.63 per serving</b>, this recipe <b>covers 33%</b> of your daily requirements of vitamins and minerals. It will be a hit at your <b>The Fourth Of July</b> event. If you have olive oil, sea salt, garlic, and a few other ingredients on hand, you can make it. To use up the olive oil you could follow this main course with the <a href="https://spoonacular.com/recipes/sauteed-banana-granola-and-yogurt-parfait-624619">Sauteed Banana, Granolan and Yogurt Parfait</a> as a dessert. Plenty of people made this recipe, and 152 would say it hit the spot. It works well as a main course.',
-   ingredients: ["1 tbsp butter", "2 tbsp grated cheese (I used romano)", "1-2 tbsp extra virgin olive oil", "5-6 cloves garlic", "6-8 ounces pasta (I used linguine)"],
-   instructions: [
-     {
-         "number": 1,
-         "step": "Preheat the oven to 200 degrees F."
-     },
-     {
-         "number": 2,
-         "step": "Whisk together the flour, pecans, granulated sugar, light brown sugar, baking powder, baking soda, and salt in a medium bowl."
-     },
-     {
-         "number": 3,
-         "step": "Whisk together the eggs, buttermilk, butter and vanilla extract and vanilla bean in a small bowl."
-     }
-   ]
- },
- { 
-  id: 4,
-  title: 'Chicken Caesar Salad with Grilled Romaine',
-  readyInMinutes: 10,
-  image: 'https://spoonacular.com/recipeImages/593906-312x231.jpg',
-  imageType: 'jpg',
-  summary:
-  'The recipe Chicken Caesar Salad with Grilled Romaine is ready <b>in roughly 10 minutes</b> and is definitely a spectacular <b>gluten free and primal</b> option for lovers of American food. One serving contains <b>559 calories</b>, <b>18g of protein</b>, and <b>48g of fat</b>. For <b>$2.63 per serving</b>, this recipe <b>covers 33%</b> of your daily requirements of vitamins and minerals. It will be a hit at your <b>The Fourth Of July</b> event. If you have olive oil, sea salt, garlic, and a few other ingredients on hand, you can make it. To use up the olive oil you could follow this main course with the <a href="https://spoonacular.com/recipes/sauteed-banana-granola-and-yogurt-parfait-624619">Sauteed Banana, Granolan and Yogurt Parfait</a> as a dessert. Plenty of people made this recipe, and 152 would say it hit the spot. It works well as a main course.',
-   ingredients: ["1 tbsp butter", "2 tbsp grated cheese (I used romano)", "1-2 tbsp extra virgin olive oil", "5-6 cloves garlic", "6-8 ounces pasta (I used linguine)"],
-   instructions: [
-     {
-         "number": 1,
-         "step": "Preheat the oven to 200 degrees F."
-     },
-     {
-         "number": 2,
-         "step": "Whisk together the flour, pecans, granulated sugar, light brown sugar, baking powder, baking soda, and salt in a medium bowl."
-     },
-     {
-         "number": 3,
-         "step": "Whisk together the eggs, buttermilk, butter and vanilla extract and vanilla bean in a small bowl."
-     }
-   ]
- }
-]
 const getItemAsync = async () => {
   const item = await AsyncStorage.getItem('favourites')
   const obj = JSON.parse(item);
@@ -109,9 +12,21 @@ const getItemAsync = async () => {
   return obj;
 }
 
+const pushRecipes = (state) => {
+  const keys = Object.keys(state)
+  let results = [];
+
+  for ( let item of keys.filter((x)=> x!== "recipeId")) {
+    results.push(state[item]);
+  }
+  return results;
+}
+
 
 export default function Favourites (props){
 const [state, setState] = useState("")
+const recipes = pushRecipes(state);
+
 
 
   useEffect(() => {
@@ -137,7 +52,7 @@ const [state, setState] = useState("")
             </TouchableOpacity>
           )}
           titleStyle={{ color:'white', fontSize:25 }}/>
-          <Button onPress={()=>{console.log(state)}}/>
+          <Button onPress={()=>{console.log(keys)}}/>
         <ScrollView contentContainerStyle={styles.cards}>
           <Block flex space="between">
             {recipes && recipes.map((recipe, id) => (
